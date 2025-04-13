d608fd6f6de6d3bf6b0303431c4b1e1c7f809f23
jsridha <jay.sridharan@gmail.com>
2025-04-13 15:29:32 -0400

    through 6.3.1.2

--- Kambaz/Users/routes.js
+++ Kambaz/Users/routes.js
@@ -3,43 +3,69 @@ import * as courseDao from "../Courses/dao.js";
 import * as enrollmentsDao from "../Enrollments/dao.js";
 
 export default function UserRoutes(app) {
-    const createUser = (req, res) => {};
+    const createUser = async (req, res) => {
+        const user = await dao.createUser(req.body);
+        res.json(user);
+    };
     app.post("/api/users", createUser);
 
-    const deleteUser = (req, res) => {};
+    const deleteUser = async (req, res) => {
+        const status = await dao.deleteUser(req.params.userId);
+        res.json(status);
+    };
     app.delete("/api/users/:userId", deleteUser);
 
-    const findAllUsers = (req, res) => {};
+    const findAllUsers = async (req, res) => {
+        const { role, name } = req.query;
+        if (role) {
+            const users = await dao.findUsersByRole(role);
+            res.json(users);
+            return;
+        }
+        if (name) {
+            const users = await dao.findUsersByPartialName(name);
+            res.json(users);
+            return;
+        }
+
+        const users = await dao.findAllUsers();
+        res.json(users);
+    };
     app.get("/api/users", findAllUsers);
 
-    const findUserById = (req, res) => {};
+    const findUserById = async (req, res) => {
+        const user = await dao.findUserById(req.params.userId);
+        res.json(user);
+    };
     app.get("/api/users/:userId", findUserById);
 
-    const updateUser = (req, res) => {
-        const userId = req.params.userId;
+    const updateUser = async (req, res) => {
+        const { userId } = req.params;
         const userUpdates = req.body;
-        dao.updateUser(userId, userUpdates);
-        const currentUser = dao.findUserById(userId);
-        req.session["currentUser"] = currentUser;
+        await dao.updateUser(userId, userUpdates);
+        const currentUser = req.session["currentUser"];
+        if (currentUser && currentUser._id === userId) {
+            req.session["currentUser"] = { ...currentUser, ...userUpdates };
+        }
         res.json(currentUser);
     };
     app.put("/api/users/:userId", updateUser);
 
-    const signup = (req, res) => {
-        const user = dao.findUserByUsername(req.body.username);
+    const signup = async (req, res) => {
+        const user = await dao.findUserByUsername(req.body.username);
         if (user) {
             res.status(400).json({ message: "Username already in use" });
             return;
         }
-        const currentUser = dao.createUser(req.body);
+        const currentUser = await dao.createUser(req.body);
         req.session["currentUser"] = currentUser;
         res.json(currentUser);
     };
     app.post("/api/users/signup", signup);
 
-    const signin = (req, res) => {
+    const signin = async (req, res) => {
         const { username, password } = req.body;
-        const currentUser = dao.findUserByCredentials(username, password);
+        const currentUser = await dao.findUserByCredentials(username, password);
         if (currentUser) {
             req.session["currentUser"] = currentUser;
             res.json(currentUser);
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import cors from "cors";
 import "dotenv/config";
 import express from "express";
 import session from "express-session";
+import mongoose from "mongoose";
 
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
@@ -11,6 +12,9 @@ import EnrollmentRoutes from "./Kambaz/Enrollments/routes.js";
 import ModuleRoutes from "./Kambaz/Modules/routes.js";
 import AssignmentRoutes from "./Kambaz/Assignments/routes.js";
 
+const CONNECTION_STRING =
+    process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz";
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(
     cors({
