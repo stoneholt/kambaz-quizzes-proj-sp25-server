f9a3de03c47b1709c5bfb1a8280ba116306e2a98
jsridha <jay.sridharan@gmail.com>
2025-04-16 20:53:34 -0400

    Started migration to mongoose

---
+++ Kambaz/Attempts/schema.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema(
+    {
+        _id: String,
+        timestamp: {
+            type: Date,
+            default: Date.now,
+            required: true,
+        },
+        user_id: {
+            type: String,
+            ref: "UserModel",
+        },
+        quiz_id: {
+            type: String,
+            ref: "QuizModel",
+        },
+        answers: {
+            type: Map,
+            of: mongoose.Schema.Types.Mixed, // allows string, boolean, etc.
+        },
+        score: {
+            type: Number,
+            min: 0,
+        },
+    },
+    {
+        collection: "attempts",
+    }
+);
+
+export default schema;
---
+++ Kambaz/Questions/schema.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema(
+    {
+        _id: String,
+        title: String,
+        type: {
+            type: String,
+            enum: ["multiple-choice", "true-false", "fill-in-the-blank"],
+            required: true,
+        },
+        points: Number,
+        answers: {
+            type: [String],
+            default: [],
+            required: true,
+        },
+        correctAnswer: {
+            type: String,
+            required: true,
+        },
+        description: String,
+    },
+    {
+        collection: "questions",
+    }
+);
+
+export default schema;
